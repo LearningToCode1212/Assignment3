e90f9e06ed71763a805b8bab432f72bfd0e47137
LearningToCode1212 <124621733+LearningToCode1212@users.noreply.github.com>
2023-12-08 14:06:04 +0200

    Final Commit

---
+++ Assignment3Sln/Assignment3/Door.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Door
+    {
+        private int Size;
+        private int Length;
+        private bool Opened;
+        private bool Closed;
+        public Door(int size, int length, bool open, bool close)
+        {
+            Size = size;
+            Length = length; 
+            Opened = open;
+            Closed = close;
+        }
+        public bool open()
+        {
+            return Opened;
+        }
+        public bool close()
+        {
+            return Closed;
+        }
+    }
+}
--- Assignment3Sln/Assignment3/Program.cs
+++ Assignment3Sln/Assignment3/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Door door1 = new Door(123, 23, false, true);
+            Door door2 = new Door(13, 53, false, false);
+
+            Console.WriteLine(door1.close());
+            Console.WriteLine(door2.close());
+            Console.WriteLine(door1.open());
+            Console.WriteLine(door2.close());
         }
     }
 }
